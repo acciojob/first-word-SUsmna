1631de91b3929a6eb21ecb4bd4e472df3a9be4f6
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-31 11:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ function firstWord(s) {
 	}
 	return s.substring(0,spaceIndex);
 }
-
+console.log(firstWord('see and stop'));
+console.log(firstWord('Hello World!'));
+console.log(firstWord('12345'));
 // Do not change the code below
 
 const s = prompt("Enter String:");
